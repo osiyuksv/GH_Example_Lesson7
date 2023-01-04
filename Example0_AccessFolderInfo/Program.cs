2ef5377bb7a8a300ebb25a31408e692515394805
Sergey Osiyuk <osiyuksv@mail.ru>
2023-01-04 12:01:47 +0300

    Example0_AccessFolderInfo

---
+++ Example0_AccessFolderInfo/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string path = "C:/Users/admin/Documents/ЦЗН Продукт менеджмент/01 Введение в программирование";
+DirectoryInfo di = new DirectoryInfo(path);
+System.Console.WriteLine(di.CreationTime);
