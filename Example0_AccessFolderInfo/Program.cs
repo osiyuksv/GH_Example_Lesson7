96a774d54c0579182c12bc7115f49b4c20890080
Sergey Osiyuk <osiyuksv@mail.ru>
2023-01-04 12:12:41 +0300

    Example0_AccessFolderInfo 02

--- Example0_AccessFolderInfo/Program.cs
+++ Example0_AccessFolderInfo/Program.cs
@@ -3,3 +3,10 @@
 string path = "C:/Users/admin/Documents/ЦЗН Продукт менеджмент/01 Введение в программирование";
 DirectoryInfo di = new DirectoryInfo(path);
 System.Console.WriteLine(di.CreationTime);
+
+FileInfo[] fi = di.GetFiles();
+
+for (int i = 0; i < fi.Length; i++)
+{
+  System.Console.WriteLine(fi[i].Name);
+}
