4e81b543ac1eb1b6dfc65cae41d1f9ad80b30438
Sergey Osiyuk <osiyuksv@mail.ru>
2023-01-04 11:30:35 +0300

    Example0_WordSearchRecurcy

---
+++ Example0_WordSearchRecurcy/Program.cs
@@ -0,0 +1,20 @@
+﻿// Перебрать возможные слова из заданных 
+// четырёх букв "а" "и" "с" "в", где в слове Т букв
+
+// char[] s = {'а', 'и', 'с', 'в'};
+int n = 1;
+
+void FindWords (string alphabet, char[] word, int length = 0)
+{
+  if(length == word.Length)
+  {
+     Console.WriteLine($"{n++} {new String(word)}"); return;
+  }
+    for (int i = 0; i < alphabet.Length; i++)
+    {
+      word[length] = alphabet[i];
+      FindWords (alphabet, word, length + 1);
+    }
+  }
+
+FindWords ("аисв", new char[5]);
